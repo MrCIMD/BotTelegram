14423f96fa064b798610437634a2cc7998259b39
Caleb Isaac Mora Díaz <calexico659@gmail.com>
2020-03-31 19:05:29 -0500

    Se hace vision de las comisiones en el parque

--- bot.py
+++ bot.py
@@ -10,21 +10,20 @@ def start(bot, update):
     update.message.reply_text('Bienvenido a BotMysql, ¿En que te puedo ayudar? Para ver mis comandos usa: /help')
 
 def help(bot, update):
-    update.message.reply_text('estos son mis comandos:\n'\
-                                'Tablas de Cargo: /cargo\n'\
-                                'Tablas de Almacenes: /almacenes\n'\
-                                'Tablas de Autores: /autores\n'\
-                                'Tablas de Clientes: /clientes\n'\
-                                'Tablas de Detalles Nota: /detalles\n')
-def cargo(bot, update):
-    conexion = pymysql.connect('localhost:3306','root','12345678','ut')
+    update.message.reply_text('Estos son mis comandos:\n'\
+                                'Tablas de Comisiones: /comisiones\n')
+
+def comisiones(bot, update):
+    update.message.reply_text("Preparando la consulta")
+    conexion = pymysql.connect('mysql.apexhosting.gdn','apexMC267066','8a18b699e0','apexMC267066')
     cur = conexion.cursor()
-    consulta = "SELECT * FROM cargo"
+    consulta = "SELECT * FROM Comisiones"
     cur.execute(consulta)
+    update.message.reply_text('[Servicio, Costo, Comision]')
     for row in cur.fetchall():
-        resultado = row [0], row[1]
+        resultado = str(row [0]), str(row[1]), str(row[2])
         update.message.reply_text(resultado)
-    print ("tu resultado de cargo")
+    print ("Tu resultado de Comisiones")
 
 def echo(bot, update):
     update.message.reply_text(update.message.text)
@@ -33,14 +32,19 @@ def error(bot, update, error):
     logger.warn('update "%s" caused error "%s"' % (update, error))
 
 def main():
-    print("Preparando el bot")
     updater = Updater("1148408397:AAEcxp4T0wzH55Le00lz0ecHdM5NKS1R_fc")
     dp = updater.dispatcher
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("comisiones", comisiones))
+
     dp.add_handler(MessageHandler(Filters.text, echo))
+
     dp.add_error_handler(error)
+
     updater.start_polling()
+
     updater.idle()
 
-main()
+if __name__ == "__main__":
+    main()
